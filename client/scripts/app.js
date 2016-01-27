3d7548d5182261722a254388e2b9b04d5a24828b
Jisoo Yoon <jyoon006@gmail.com>
2016-01-27 01:01:45 -0500

    code cleanup/added comments

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,37 +1,33 @@
 var app;
 $(document).ready(function() {
 
-
-
-
-
- app = {
+//initializing app object
+app = {
    server: 'https://api.parse.com/1/classes/chatterbox',
    username: 'anonymous',
    roomname: 'lobby',
    friends: {},
-
+//init method is ran when document is loaded
   init: function() {
     app.username = window.location.search.split("=")[1];
-    //app.username = window.location.search.substr(10);
-    // var mostRecentMessage = '?order=-createdAt';
     this.fetch();
 
-    app.$main = $('#main');
     app.$message = $('#messageText');
     app.$chats = $('#chat');
     app.$roomSelect = $('#roomSelect');
-    app.$send = $('#send');
-
-
+//on submit of form, invoke handleSubmit method with preventing default event handler due to issues
     $('form').on('submit', function(e) {
       e.preventDefault();
       app.handleSubmit();
     });
+//when room is changed, save the room
     $('#roomSelect').on('change', app.saveRoom);
+//when username is clicked invoke addFriend method
     $('#main').on('click', '.username', app.addFriend);
     setInterval(app.fetch, 10000);
   },
+
+//make ajax call as POST to add data to the server
   send: function(message) {
     $('.chat').empty();
     $('#messageText').val("");
@@ -49,7 +45,7 @@ $(document).ready(function() {
       }
     });
   },
-
+//make ajax call to GET data from server
   fetch: function() {
     $.ajax({
       url: app.server,
@@ -70,13 +66,14 @@ $(document).ready(function() {
       }
     });
   },
+//iterate through data from server and invoke addMessage method with every object
   populateMessage: function(data) {
     app.clearMessages();
     for (var i = 0; i < data.length; i++) {
-      
       app.addMessage(data[i]);
     }
   },
+//iterate through data from server and get the roomnames from the data and invoke addRoom method
   populateRoom: function(data) {
     app.$roomSelect.html('<option value="__newRoom">New room...</option><option value="" selected>lobby</option></select>');
     var roomHolder = {};
@@ -90,9 +87,11 @@ $(document).ready(function() {
     }
     $('#roomSelect').val(app.roomname);
   },
+  //delete all messages
   clearMessages: function() {
     $('#chat').empty();
   },
+  //append users message to dom depending on the chatroom they're in
   addMessage: function(data) {
     
     if(!data.roomname) {
@@ -118,61 +117,61 @@ $(document).ready(function() {
 
     app.$chats.append($chat);
     
-    // $('.username').on('click', function() {
-    //   app.addFriend();
-    // });
+   
     }
   },
+  //add room names to room's list in DOM
   addRoom: function(roomname) {
     var $option = $('<option/>').val(roomname).text(roomname);
-    // $('#roomSelect').append('<option value="' + roomname + '">' + roomname + '</option>');
+    
     app.$roomSelect.append($option);
      
   },
+  //add friend when click handler is invoked on the div element that contains user's name
   addFriend: function(evt) {
     var username = $(evt.currentTarget).attr('data-username');
       if (username !== undefined) {
         console.log('chatterbox: Adding %s as a friend', username);
 
-        // Store as a friend
+        
         app.friends[username] = true;
 
-        // Bold all previous messages
-        // Escape the username in case it contains a quote
+        
         var selector = '[data-username="'+username.replace(/"/g, '\\\"')+'"]';
         var $usernames = $(selector).addClass('friend');
       }
 
   },
+  //save a new room when user creates one by invoking new room name to addRoom method
   saveRoom: function(evt) {
 
       var selectIndex = app.$roomSelect.prop('selectedIndex');
-      // New room is always the first option
+      
       if (selectIndex === 0) {
         var roomname = prompt('Enter room name');
         if (roomname) {
-          // Set as the current room
+          
           app.roomname = roomname;
 
-          // Add the room to the menu
+          
           app.addRoom(roomname);
 
-          // Select the menu option
+          
           app.$roomSelect.val(roomname);
 
-          // Fetch messages again
+          
           app.fetch();
         }
       }
       else {
-        // app.startSpinner();
-        // Store as undefined for empty names
+        
         app.roomname = app.$roomSelect.val();
 
-        // Fetch messages again
+        
         app.fetch();
       }
     },
+//invokes data to send method to make AJAX call to POST to server data
   handleSubmit: function() {
     var message = {
       username: app.username,
@@ -185,10 +184,7 @@ $(document).ready(function() {
   }
 };
 
-
   app.init();
-
-
 
 });
 
